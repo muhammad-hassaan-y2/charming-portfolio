80228bd5a4b7cd6c5277a6746acc93f51c2660f9
Muhammad Hassaan Shaukat <mr.hassaan50@gmail.com>
2024-01-15 22:40:46 +0500

    Project Startup

---
+++ src/slices/Hero/index.tsx
@@ -0,0 +1,29 @@
+import { Content } from "@prismicio/client";
+import { SliceComponentProps } from "@prismicio/react";
+
+/**
+ * Props for `Hero`.
+ */
+export type HeroProps = SliceComponentProps<Content.HeroSlice>;
+
+/**
+ * Component for "Hero" Slices.
+ */
+const Hero = ({ slice }: HeroProps): JSX.Element => {
+  return (
+    <section
+      data-slice-type={slice.slice_type}
+      data-slice-variation={slice.variation}
+    >
+   
+
+   <span className="text-red-500">{slice.primary.first_name}</span>
+   <>{slice.primary.last_name}</>
+   <>{slice.primary.tagline}</>
+   
+
+    </section>
+  );
+};
+
+export default Hero;
